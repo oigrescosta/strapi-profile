4b8748b4f80a611ffc3ade81833b7c2a0954bd0f
unknown <costa.sergio.86@gmail.com>
2021-03-26 15:10:18 +0000

    Creating Login/Register and Profile component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import RegisterOrLogin from './components/RegisterOrLogin'
+import ProfilePage from './components/ProfilePage'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    user: null
+  }
+  render() {
+    const {user} = this.state
+    return(
+      <div className="App">
+        {!user &&
+          <RegisterOrLogin updatedUser={(user) => this.setState({user})} />
+        }
+        {user &&
+          <ProfilePage user={user} />
+        }
+     </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/components/RegisterOrLogin.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import axios from 'axios'
+
+class RegisterOrLogin extends React.Component {
+    state = {
+        email: '',
+        password: '',
+        mode: 'login'
+    }
+
+    handleChange = (e) => {
+        const {name, value} = e.target
+        this.setState({[name]: value})
+
+        console.log("RegisterOrLogin.handleChange e.target.name", name)
+        console.log("RegisterOrLogin.handleChange e.target.value", value)
+    }
+
+    handleSubmit = async (e) => {
+        e.preventDefault()
+        console.log("RegisterOrLogin.handleSubmit")
+        //Sign the user up with Strapi
+        const {email, password, mode} = this.state
+
+        const data = {
+            email, 
+            password,
+            username: email,
+            identifier: email
+        }
+
+        let url = ''
+        if(mode === 'login') {
+            url = 'http://localhost:1337/auth/local'
+        }
+
+        if(mode === 'signup') {
+            url = 'http://localhost:1337/auth/local/register'
+        }
+
+        const userCreationRes = await axios({
+            method: 'POST',
+            url,
+            data
+        })
+
+        console.log("RegisterOrLogin.handleSubmit userCreationRes", userCreationRes)
+        if(this.props.updatedUser && typeof this.props.updatedUser === 'function') {
+            this.props.updatedUser(userCreationRes.data)
+        }
+    }
+
+    render() {
+        const {email, password, mode} = this.state
+        return(
+            <div className="RegisterOrLogin">
+                <h1>{mode}</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <label htmlFor="email">Email</label>
+                        <input onChange={this.handleChange} name="email" id="email" value={email} />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" onChange={this.handleChange} name="password" id="password" value={password} />
+                    </div>
+                    <button type="submit">{mode}</button>
+                </form>
+                {mode === 'login' && 
+                <p onClick={() =>  this.setState({mode:'signup'})}>Want to Signup instead?</p>}
+                {mode === 'signup' && 
+                <p onClick={() =>  this.setState({mode:'login'})}>Want to Login instead?</p>}
+            </div>
+        )
+    }
+}
+
+export default RegisterOrLogin
